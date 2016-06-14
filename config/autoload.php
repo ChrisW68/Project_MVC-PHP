799b1c80ff1fb110975fc0e4b61b819b938143e1
Chris <chris.wiseman74@gmail.com>
2016-06-14 12:28:32 -0700

    Added login logout dialogue and password verification

--- config/autoload.php
+++ config/autoload.php
@@ -58,7 +58,7 @@ $autoload['packages'] = array();
 |
 |	$autoload['libraries'] = array('user_agent' => 'ua');
 */
-$autoload['libraries'] = array('database');  //database class is being used
+$autoload['libraries'] = array('database', 'form_validation', 'session');  //database class is being used
 
 /*
 | -------------------------------------------------------------------
--- views/users/login_view.php
+++ views/users/login_view.php
@@ -1,7 +1,20 @@
+<?php if($this->session->userdata('logged_in')): ?>
+
+<?php echo "logout"; ?>
+
+<?php else: ?>
+
 <h2>Login form</h2>
 
 
+
 <?php $attributes = array('id'=>'login_form', 'class'=>'form_horizontal'); ?>
+
+<?php if($this->session->flashdata('errors')): ?>
+
+<?php echo $this->session->flashdata('errors'); ?>
+
+<?php endif; ?>
 
 
 <?php echo form_open('users/login', $attributes); ?>
@@ -38,6 +51,21 @@ $data = array(
 
 <div class="form-group">
 
+
+<?php echo form_label('Confirm Password'); ?>
+<?php
+	$data = array(
+	'class'=> 'form_control',
+	'name'=> 'confirm_password',
+	'placeholder'=> 'Confirm Password'
+); ?>
+
+<?php echo form_password($data); ?>
+
+</div>
+
+<div class="form-group">
+
 <?php
 	$data = array(
 	'class' => 'btn btn-primary',
@@ -50,3 +78,7 @@ $data = array(
 </div>
 
 <?php echo form_close(); ?>
+
+
+
+<?php endif; ?>
